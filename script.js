98b8674b25f7ad1c24f66f4c549b116ab3a3ad17
Ricardo Jaen <jaenricardo300@gmail.com>
2025-04-11 18:18:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll('#category-index button');
+const cards = document.querySelectorAll('.image-card');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const category = button.dataset.category;
+
+    cards.forEach(card => {
+      if (category === 'all' || card.classList.contains(category)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+});
+
+document.querySelector('[data-category="all"]').click();
